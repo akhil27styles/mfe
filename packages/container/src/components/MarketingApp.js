3ca99b05c52874448422b07f6cde7d6fd4fbe3d4
Singh <akhil-kumar.singh@capgemini.com>
2025-06-08 17:29:19 +0530

    'test-workflow'

--- packages/container/src/components/MarketingApp.js
+++ packages/container/src/components/MarketingApp.js
@@ -1,6 +1,8 @@
 import { mount } from "../../../marketing/src/bootstrap";
 import React,{useRef,useEffect} from "react";
 
+
+
 export default ()=>{
     const ref = useRef(null);
     useEffect(()=>{
