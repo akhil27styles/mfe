577f04b653ee0de7270a1ede07c1cb99a2ac613e
Singh <akhil-kumar.singh@capgemini.com>
2025-06-09 17:13:37 +0530

    added marketing

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
   return (
     <div>
-      <h1>Hi there I am there to!</h1>
+      <h1>Hi there I am there to which!</h1>
       <hr />
       <MarketingApp />
     </div>
